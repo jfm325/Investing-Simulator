84bc278f81f9de090a0ea94036f999c08ae418d1
Joey Morquecho <jfm325@cornell.edu>
2021-03-29 15:04:41 -0400

    Update makefile with make timer

--- timer.ml
+++ timer.ml
@@ -1,14 +1,14 @@
-open Core
+(* open Core *)
 open Async
 
 let rec timer () =
-  after (sec 5.)
-  >>= fun () ->
-    (*call func to read next line of prices*)
+  after (Core.sec 5.) >>= fun () ->
+  (*call func to read next line of prices*)
   print_endline "STOCK PRICES ARE NOW UPDATED";
-  timer () 
+  timer ()
 
-let () = let cmd = Command.async ~summary:"Periodic timer"
-   Command.Spec.empty timer in 
-  Command.run cmd 
-  
+let () =
+  let cmd =
+    Command.async ~summary:"Periodic timer" Command.Spec.empty timer
+  in
+  Command.run cmd
