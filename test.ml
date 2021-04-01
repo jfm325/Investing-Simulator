3d30d7dd550d87d003efe16aaf409c2b81449bfa
Anushka <am2265@cornell.edu>
2021-04-01 02:23:22 -0400

    Added test for interaction

--- test.ml
+++ test.ml
@@ -1,6 +1,7 @@
 open OUnit2
 open Stock
 open User
+open Interaction
 
 (* Printers *)
 
@@ -17,6 +18,24 @@ let stock_test test_name (stock : Stock.t) (name : string)
   assert_equal ticker (get_ticker stock) ~printer:string_of_s;
   assert_equal price (get_price stock index) ~printer:string_of_float
 
+let interaction_tests =
+  [
+    (* Added tests for the Interaction module here *)
+    ( "Testing for buying shares" >:: fun _ ->
+      assert_equal (Buy [ "COKE"; "50" ]) (parse "buy COKE 50") );
+    ( "Testing for selling shares" >:: fun _ ->
+      assert_equal (Sell [ "COKE"; "50" ]) (parse "sell COKE 50") );
+    ("Testing for cash" >:: fun _ -> assert_equal Cash (parse "cash"));
+    ( "Testing for networth" >:: fun _ ->
+      assert_equal Networth (parse "networth") );
+    ( "Testing for Empty" >:: fun _ ->
+      assert_raises EmptyCommand (fun () -> parse "") );
+    ( "Testing for Empty" >:: fun _ ->
+      assert_raises EmptyCommand (fun () -> parse " ") );
+    ( "Testing for Malformed" >:: fun _ ->
+      assert_raises BadCommand (fun () -> parse "hi i am confused") );
+  ]
+
 let stock_tests =
   let coke = Stock.create_stock "Coke" "COKE" "coke.txt" in
   [
@@ -28,6 +47,7 @@ let stock_tests =
       62.98 200;
   ]
 
-let suite = "test suite 1" >::: List.flatten [ stock_tests ]
+let suite =
+  "test suite 1" >::: List.flatten [ stock_tests; interaction_tests ]
 
 let _ = run_test_tt_main suite
