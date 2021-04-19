6a3b2cf781a188c440cf5232f13cbd42b530c8f2
Joey Morquecho <jfm325@cornell.edu>
2021-04-19 17:02:50 -0400

    Fix year and month calculations in game.ml

--- game.ml
+++ game.ml
@@ -6,18 +6,22 @@ let get_start_time () = !start_time
 
 let game_ended s =
   let current_time = int_of_float (Unix.time () -. !start_time) in
-  let month = current_time / s in
-  let year =
-    if month = 0 then 1
-    else if month mod 12 = 0 then month / 12
-    else (month / 12) + 1
+  let index = current_time / s in
+  let month =
+    let m = index + 1 in
+    if m mod 12 = 0 then 12 else m mod 12
   in
+  let year =
+    let m = index + 1 in
+    if m mod 12 = 0 then m / 12 else (m / 12) + 1
+  in
+  let year_str = string_of_int year in
+  let month_str = string_of_int month in
   (* End game when we reach last index. *)
-  if month >= 240 then (
+  if index >= 240 then (
     (* Should update state in main.ml *)
     print_endline "End of game";
     true )
   else (
-    print_endline
-      ("Year " ^ string_of_int year ^ " Month " ^ string_of_int month);
+    print_endline ("Year " ^ year_str ^ " Month " ^ month_str);
     false )
