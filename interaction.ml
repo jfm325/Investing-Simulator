8a8a922e2fa54fb08a15cf9de62464bcdabd7519
Anushka <am2265@cornell.edu>
2021-05-21 18:36:15 -0400

    cleaned interction

--- interaction.ml
+++ interaction.ml
@@ -136,118 +136,135 @@ let give_user_income_if_needed u =
     times_income_received := income_times )
   else ()
 
+let botnet curr =
+  let botnw = string_of_float (Bot.get_net_worth bot) in
+  print_string ("The networth of the bot is " ^ curr ^ botnw ^ "\n")
+
+let display_index u =
+  let p = User.get_portfolio u in
+  let in_h = Portfolio.get_index_history p in
+  print_in in_h
+
+let displaycd u =
+  let p = User.get_portfolio u in
+  let cd_h = Portfolio.get_cd_history p in
+  let c_lst = Cd_history.get_cd_lst cd_h in
+  print_cd cd_h c_lst 1
+
+let buy_cd u invest =
+  let amt = float_of_string (List.hd invest) in
+  if amt < 1000. then print_string "Amount not sufficient \n"
+  else
+    let term = int_of_string (List.nth invest 1) in
+    let cdterm = checklegalterm term in
+    User.buy_cd u amt cdterm;
+    print_string "You just bought a cd and your cash has changed \n"
+
+let sell_cd u invest =
+  let n = int_of_string (List.hd invest) in
+  let i = n - 1 in
+  User.sell_cd u i;
+  print_string "You just sold cd and your cash has changed \n"
+
+let user_networth u curr =
+  Stock.update_current_prices Init.stocks (Game.get_start_time ());
+  Stock.update_current_prices Init.index_funds (Game.get_start_time ());
+  let n =
+    string_of_float (User.get_net_worth u Init.stocks Init.index_funds)
+  in
+  print_string ("Your current networth is " ^ curr ^ n ^ "\n")
+
+let buy_shares u invest =
+  (* Stock.update_current_prices stocks (Game.get_start_time ()); let s
+     = List.hd invest in let n = int_of_string (List.nth invest 1) in
+     let stock = legal stocks s in if User.get_cash u -. (float n *.
+     Stock.get_current_price stock) <= 0.0 then print_string
+     "TRANSACTION ERROR: You do not have enough cash to \ purchase this
+     stock \n" else if n <= 0 then print_string "TRANSACTION ERROR: You
+     have to buy a positive number of \ shares \n" else ( User.buy_stock
+     n u stock; print_string "You just bought stocks and your cash has
+     changed \n" ) *)
+  let stock_n = int_of_string (List.hd invest) - 1 in
+  let num_of_stocks = List.length Init.stocks in
+  if stock_n < 0 || stock_n >= num_of_stocks then
+    print_string "TRANSACTION ERROR: Invalid stock number.\n"
+  else
+    let shares = int_of_string (List.nth invest 1) in
+    let stock = List.nth Init.stocks stock_n in
+    let curr_price = Stock.get_current_price stock in
+    let cost = float shares *. curr_price in
+    if User.get_cash u -. cost < 0.0 then
+      print_endline
+        "TRANSACTION ERROR: You do not have enough cash to purchase \
+         this stock \n"
+    else User.buy_stock shares u stock
+
+let sell_shares u invest =
+  Stock.update_current_prices Init.stocks (Game.get_start_time ());
+  let stock_n = int_of_string (List.hd invest) - 1 in
+  let shares = int_of_string (List.nth invest 1) in
+  let stock = List.nth Init.stocks stock_n in
+  User.sell_stock shares u stock stock_n
+
+let buy_index u invest =
+  let index_n = int_of_string (List.hd invest) - 1 in
+  let num_of_index_funds = List.length Init.index_funds in
+  if index_n < 0 || index_n >= num_of_index_funds then
+    print_string "TRANSACTION ERROR: Invalid index fund number.\n"
+  else
+    let shares = int_of_string (List.nth invest 1) in
+    let index_fund = List.nth Init.index_funds index_n in
+    let curr_price = Stock.get_current_price index_fund in
+    let cost = float shares *. curr_price in
+    if User.get_cash u -. cost < 0.0 then
+      print_endline
+        "TRANSACTION ERROR: You do not have enough cash to purchase \
+         this stock \n"
+    else (
+      User.buy_index shares u index_fund;
+      print_string
+        "You just bought into an index fund and your cash has changed\n"
+      )
+
+let sell_index u invest =
+  Stock.update_current_prices Init.index_funds (Game.get_start_time ());
+  let index_n = int_of_string (List.hd invest) - 1 in
+  let shares = int_of_string (List.nth invest 1) in
+  let index_fund = List.nth Init.index_funds index_n in
+  User.sell_index shares u index_fund index_n
+
+let checkstock_helper invest =
+  Stock.update_current_prices stocks (Game.get_start_time ());
+  let s = List.hd invest in
+  let st = legal stocks s in
+  let b = legal_stock_history stock_history_lst s in
+  let c = string_of_float (User.get_stocks_pl st b) in
+  if float_of_string c < 0. then
+    print_string
+      ("Your stocks currently has a loss $" ^ c ^ " dollars \n")
+  else
+    print_string
+      ("Your stocks currently has a gain $" ^ c ^ " dollars \n")
+
 let view com u (curr : string) =
   give_user_income_if_needed u;
   try
     match com with
-    | BotNetworth ->
-        let botnw = string_of_float (Bot.get_net_worth bot) in
-        print_string
-          ("The networth of the bot is " ^ curr ^ botnw ^ "\n")
-    | ViewIndex ->
-        let p = User.get_portfolio u in
-        let in_h = Portfolio.get_index_history p in
-        print_in in_h
-    | ViewCD ->
-        let p = User.get_portfolio u in
-        let cd_h = Portfolio.get_cd_history p in
-        let c_lst = Cd_history.get_cd_lst cd_h in
-        print_cd cd_h c_lst 1
-    | BuyCD invest ->
-        let amt = float_of_string (List.hd invest) in
-        if amt < 1000. then print_string "Amount not sufficient \n"
-        else
-          let term = int_of_string (List.nth invest 1) in
-          let cdterm = checklegalterm term in
-          User.buy_cd u amt cdterm;
-          print_string
-            "You just bought a cd and your cash has changed \n"
-    | SellCD invest ->
-        let n = int_of_string (List.hd invest) in
-        let i = n - 1 in
-        User.sell_cd u i;
-        print_string "You just sold cd and your cash has changed \n"
+    | BotNetworth -> botnet curr
+    | ViewIndex -> display_index u
+    | ViewCD -> displaycd u
+    | BuyCD invest -> buy_cd u invest
+    | SellCD invest -> sell_cd u invest
     | Help -> print_string Init.instructions
     | Cash ->
         let c = string_of_float (User.get_cash u) in
         print_string ("Your current cash is " ^ curr ^ c ^ "\n")
-    | Networth ->
-        Stock.update_current_prices Init.stocks (Game.get_start_time ());
-        Stock.update_current_prices Init.index_funds
-          (Game.get_start_time ());
-        let n =
-          string_of_float
-            (User.get_net_worth u Init.stocks Init.index_funds)
-        in
-        print_string ("Your current networth is " ^ curr ^ n ^ "\n")
-    | Buy_S invest ->
-        (* Stock.update_current_prices stocks (Game.get_start_time ());
-           let s = List.hd invest in let n = int_of_string (List.nth
-           invest 1) in let stock = legal stocks s in if User.get_cash u
-           -. (float n *. Stock.get_current_price stock) <= 0.0 then
-           print_string "TRANSACTION ERROR: You do not have enough cash
-           to \ purchase this stock \n" else if n <= 0 then print_string
-           "TRANSACTION ERROR: You have to buy a positive number of \
-           shares \n" else ( User.buy_stock n u stock; print_string "You
-           just bought stocks and your cash has changed \n" ) *)
-        let stock_n = int_of_string (List.hd invest) - 1 in
-        let num_of_stocks = List.length Init.stocks in
-        if stock_n < 0 || stock_n >= num_of_stocks then
-          print_string "TRANSACTION ERROR: Invalid stock number.\n"
-        else
-          let shares = int_of_string (List.nth invest 1) in
-          let stock = List.nth Init.stocks stock_n in
-          let curr_price = Stock.get_current_price stock in
-          let cost = float shares *. curr_price in
-          if User.get_cash u -. cost < 0.0 then
-            print_endline
-              "TRANSACTION ERROR: You do not have enough cash to \
-               purchase this stock \n"
-          else User.buy_stock shares u stock
-    | Sell_S invest ->
-        Stock.update_current_prices Init.stocks (Game.get_start_time ());
-        let stock_n = int_of_string (List.hd invest) - 1 in
-        let shares = int_of_string (List.nth invest 1) in
-        let stock = List.nth Init.stocks stock_n in
-        User.sell_stock shares u stock stock_n
-    | Buy_Index invest ->
-        let index_n = int_of_string (List.hd invest) - 1 in
-        let num_of_index_funds = List.length Init.index_funds in
-        if index_n < 0 || index_n >= num_of_index_funds then
-          print_string "TRANSACTION ERROR: Invalid index fund number.\n"
-        else
-          let shares = int_of_string (List.nth invest 1) in
-          let index_fund = List.nth Init.index_funds index_n in
-          let curr_price = Stock.get_current_price index_fund in
-          let cost = float shares *. curr_price in
-          if User.get_cash u -. cost < 0.0 then
-            print_endline
-              "TRANSACTION ERROR: You do not have enough cash to \
-               purchase this stock \n"
-          else (
-            User.buy_index shares u index_fund;
-            print_string
-              "You just bought into an index fund and your cash has \
-               changed\n" )
-    | Sell_Index invest ->
-        Stock.update_current_prices Init.index_funds
-          (Game.get_start_time ());
-        let index_n = int_of_string (List.hd invest) - 1 in
-        let shares = int_of_string (List.nth invest 1) in
-        let index_fund = List.nth Init.index_funds index_n in
-        User.sell_index shares u index_fund index_n
-    | Checkstock invest ->
-        Stock.update_current_prices stocks (Game.get_start_time ());
-        let s = List.hd invest in
-        let st = legal stocks s in
-        let b = legal_stock_history stock_history_lst s in
-        let c = string_of_float (User.get_stocks_pl st b) in
-        if float_of_string c < 0. then
-          print_string
-            ("Your stocks currently has a loss $" ^ c ^ " dollars \n")
-        else
-          print_string
-            ("Your stocks currently has a gain $" ^ c ^ " dollars \n")
+    | Networth -> user_networth u curr
+    | Buy_S invest -> buy_shares u invest
+    | Sell_S invest -> sell_shares u invest
+    | Buy_Index invest -> buy_index u invest
+    | Sell_Index invest -> sell_index u invest
+    | Checkstock invest -> checkstock_helper invest
   with Not_found ->
     print_string "Invalid stock not found in market. \n"
 
